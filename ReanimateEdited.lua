1077fcdd8d8f0046c71357076f7abaceee4aebfb
IceIXLOL <122179245+IceIXLOL@users.noreply.github.com>
2023-01-08 18:48:52 -0300

    Update ReanimateEdited.lua

--- ReanimateEdited.lua
+++ ReanimateEdited.lua
@@ -19,16 +19,9 @@ local Global = getgenv and getgenv() or _G
 	
 	
 	local function RainbowSelection(Part)
-		local Selection = Instance.new("Highlight")
-		Selection.Name = "Padero Pride Month Special"
-		Selection.Adornee = Part
-		Selection.FillTransparency = 0.7
-		Selection.DepthMode = 0
 		task.spawn(function()
 			while Character do
 				for i = 0,1,0.004 do
-					Selection.FillColor = Color3.fromHSV(i,1,1)
-					Selection.OutlineColor = Color3.fromHSV(i,1,1)
 					Part.Transparency = 0
 					if isnetworkowner(Part) then Selection.Enabled = false else Selection.Enabled = false end
 					task.wait()
